c42c744c202d4f769b46fe76fa869f74e660badc
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-10-30 07:27:55 +0600

    added loder

--- src/components/PhoneData/Phone.jsx
+++ src/components/PhoneData/Phone.jsx
@@ -1,37 +1,57 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { BarChart, Bar, XAxis, YAxis } from "recharts"
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts"
+import { Audio } from 'react-loader-spinner'
+
+
 
 function Phone() {
 
-    const [phone, setPhone] = useState([])
+    const [phone, setPhone] = useState([]);
+    const [isLoading, setLoading] = useState(true)
 
     useEffect(()=>{
-      // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-      //  .then(res=> res.json())
-      //  .then(data => setPhone(data.data))
       axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-      .then(data => {
-        const PhoneFakeData = data.data.data;
-        const newFakeData = PhoneFakeData.map(phone => {
 
+      .then(data => {
+        const allPhoneData = data.data.data;
+        const newFakeData = allPhoneData.map(phone => {
           const obj = {
               name: phone.phone_name,
               price: parseInt(phone.slug.split('-')[1])
           }
           return obj;
-        });
-       setPhone(newFakeData)
+        })
+        setPhone(newFakeData)
+        setLoading(false)
       })
+
+
+
       } ,[])
 
   return (
     <>
     <h1 className="text-7xl">Phone: {phone.length}</h1>
+    <div className="">
+      {
+        isLoading &&  <Audio
+  height="100"
+  width="100"
+  color="#4fa94d"
+  ariaLabel="audio-loading"
+  wrapperStyle={{}}
+  wrapperClass="wrapper-class"
+  visible={true}
+  />
+      }
+    </div>
+   
     <BarChart width={1240} height={400} data={phone}>
           <Bar dataKey="price" fill="green"></Bar>
           <XAxis dataKey="name" />
           <YAxis></YAxis>
+          <Tooltip />
     </BarChart>
     </>
   )
